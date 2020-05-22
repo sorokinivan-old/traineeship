dac9255d712829d5759dc006b059f29f660b4fab
sorokinivan <nextop05@gmail.com>
2020-05-22 16:19:10 +0300

    Created asp.net project

---
+++ Задание 2/ASPNETProject/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ASPNETProject.Models;
+
+namespace ASPNETProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DBContext db = new DBContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.CheckConnection = db.Database.CanConnect();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Задание 2/ASPNETProject/Models/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETProject.Models
+{
+
+    public class DBContext : DbContext
+    {
+        public static string connString = "Host=localhost;Port=5432;Database=traineeship;Username=postgres;Password=root";
+
+
+        public DBContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connString);
+        }
+    }
+}
